58d06bd58eccdee37d39cc627f63b903c0eb79eb
Leonid <lmkorepanov@gmail.com>
2020-11-09 21:14:54 +0500

    ФТ теперь проверяет возможность ввести элемент списка

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -19,18 +21,34 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # Заголовок и шапка страницы говорят о списе неотложных дел
-        self.assertIn('To-Do' in self.browser.title)
-        self.fail('Закончить тест')
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Сразу предлагается ввести элемент чписка
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Набираем в текстовом поле "Купить паввлиньи перья"
+        inputbox.send_keys('Купить павлиньи перья')
 
         # Когда нажимаем enter, страница обновляется, и теперь страница
         # содержит "1: Купить павлиньи перья"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
 
         # Текстовое поле по-прежнемк предлагает добавить еще один элемент
         # Вводим "Сделать мушку из павлиньих перьев"
+        self.fail('Закаончить тест!')
 
         # Страница снова обновляется
 
