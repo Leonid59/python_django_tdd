cb80e6b392f9eccb805b25fdec9de4198bcc6d80
Leonid <lmkorepanov@gmail.com>
2020-11-15 19:22:53 +0500

    Рефакторинг: вынес проверку строки в таблице

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,13 @@ class NewVisitorTest(unittest.TestCase):
         """демонтаж"""
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """подтверждение строки в таблице списка"""
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """тест: можно начать список и получить его позже"""
         # Домашняя страница приложения со списком дел
@@ -40,9 +47,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
 
         # Текстовое поле по-прежнемк предлагает добавить еще один элемент
         # Вводим "Сделать мушку из павлиньих перьев"
@@ -52,10 +57,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # Страница снова обновляется
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
-        self.assertIn('2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 
         # Видим, что сайт сгенерировал уникальный URL-адрес - об этом
         # выводится сообщениее
